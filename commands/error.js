af8792e9dc5863533078324e7878218d545a27b1
nysteo <teonys@nyu.edu>
2020-09-14 16:42:14 -0400

    test

--- commands/error.js
+++ commands/error.js
@@ -6,6 +6,6 @@ module.exports = {
     async run (bot, message, args) {
 
 
-        message.channel.send("Let me put it this way, Mr. Amor. The 9000 series is the most reliable computer ever made. No 9000 computer has ever made a mistake or distorted information. We are all, by any practical definition of the words, foolproof and incapable of error.");
+        message.channel.send("Let dme put it this way, Mr. Amor. The 9000 series is the most reliable computer ever made. No 9000 computer has ever made a mistake or distorted information. We are all, by any practical definition of the words, foolproof and incapable of error.");
     }
 }
